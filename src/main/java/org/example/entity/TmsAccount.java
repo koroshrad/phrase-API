f72371f63def3efcab1be43ee8de5cfd6a484248
Korosh <koroshrad@yahoo.co.uk>
2023-02-15 16:59:08 +0000

    Initial commit

---
+++ src/main/java/org/example/entity/TmsAccount.java
@@ -0,0 +1,21 @@
+package org.example.entity;
+
+import lombok.Data;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/org/example/repo/TmsAccountRepository.java
@@ -0,0 +1,7 @@
+package org.example.repo;
+
+import org.example.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
